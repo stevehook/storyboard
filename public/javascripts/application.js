482ffdc44460718dc4d77a8c2002f2d3dcfe5882
Steve Hook <steve.hook@gmail.com>
2011-01-27 19:41:30 +0000

    Edit a story from the /stories list

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+// TODO: Will need to generalise this into some kind of plugin for lists at some stage in the future...
+$(function() {
+  var tbody = $('.listTable tbody');
+  tbody.click(function(event) {
+    var node = event.target;
+    while (node && node.tagName != 'TR') {
+      node = node.parentNode;
+    }
+    if (node) { window.location = $(node).attr('data-href'); }
+  });
+});
