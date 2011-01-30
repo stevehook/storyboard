67d1824b2c9059412b308ff861184c6961c33009
Steve Hook <steve.hook@gmail.com>
2011-01-30 11:15:24 +0000

    Better way to change the formatting of spec output using .rspec file

--- lib/tasks/specdoc.rake
+++
@@ -1,6 +0,0 @@
-require 'rspec/core/rake_task'
-
-RSpec::Core::RakeTask.new do |t|
-  t.name = 'specdoc'
-  t.rspec_opts = ["--color", "--format", "documentation"]
-end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,30 +1,26 @@
 require 'spec_helper'
 
 describe Project do
-  before(:each) do
-  end
-
-  after(:each) do
-  end
+  let(:project) { Project.create(:title => 'test project', :start_date => Time.new) }
    
   it "should be valid if mandatory attributes are specified" do
-    project = Project.create(:title => 'test title', :description => 'test description')
     project.valid?.should be_true
     project.errors[:title].none?.should be_true
   end
   
   context "after creating new Project with one sprint" do
-    before(:each) do
-      @project = Project.create(:title => 'test project')
-      @sprint = @project.add_sprint
-    end
+    let!(:sprint) { project.add_sprint }
     
     it "should have 1 sprint in the collection" do
-      @project.sprints.length.should == 1
+      project.sprints.length.should == 1
     end
     
-    it "should have 1 sprint in the collection" do
-      @sprint.order.should == 1
+    it "sprint should have order 1" do
+      sprint.order.should == 1
+    end
+    
+    it "sprint should have correct start time" do
+      # TODO
     end
   end
 end
