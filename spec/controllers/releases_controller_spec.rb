cb2f3bded40d4fbc2ec81a56bc9e5ff031016f80
Steve Hook <steve.hook@gmail.com>
2011-02-06 21:20:05 +0000

    Added simple controller spec - more to follow

--- spec/controllers/releases_controller_spec.rb
+++ spec/controllers/releases_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe ReleasesController do
-
+  describe "GET index" do 
+    let(:release) { Release.new(:title => 'Mock Release 1') }
+    
+    it "assigns all releases as @releases" do
+      Release.stub(:all) { [release] } 
+      get :index 
+      assigns(:releases).should eq([release])
+    end 
+  end
 end
