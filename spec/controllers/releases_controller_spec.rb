71f4e1b1539e53ecceb09bccfb320d6ea4484dde
Steve Hook <steve.hook@gmail.com>
2011-02-06 21:24:54 +0000

    A couple of additional specs for the releases_controller

--- spec/controllers/releases_controller_spec.rb
+++ spec/controllers/releases_controller_spec.rb
@@ -4,10 +4,20 @@ describe ReleasesController do
   describe "GET index" do 
     let(:release) { Release.new(:title => 'Mock Release 1') }
     
+    it "should be successful" do
+      get :index
+      response.should be_success
+    end
+    
+    it "should render the index view" do
+      get :index
+      response.should render_template(:index)
+    end
+    
     it "assigns all releases as @releases" do
-      Release.stub(:all) { [release] } 
-      get :index 
-      assigns(:releases).should eq([release])
-    end 
+      Release.stub(:all) { [release] }
+      get :index
+      assigns(:releases).should == [release]
+    end
   end
 end
