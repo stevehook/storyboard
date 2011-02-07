367dc5fde1ef638d09dcb31b5aa49cde6a7a22ed
Steve Hook <steve.hook@gmail.com>
2011-02-07 21:17:47 +0000

    Improved the release#show view

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Storyboard::Application.initialize!
+
+Time::DATE_FORMATS[:date] = "%A %d %B"
+Date::DATE_FORMATS[:date] = "%A %d %B"
