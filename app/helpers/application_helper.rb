772fb3c6b8574536ead12ce6320add1274fa8192
Steve Hook <steve.hook@gmail.com>
2011-02-16 21:04:06 +0000

    Standardised the use rendering of page titles so that same text appears in the browser title and title within the document

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def page_title
+    # TODO: Use this in the head title - doesn't seem to work properly...
+    (@content_for_title + " &mdash; " if @content_for_title).to_s + 'Storyboard'
+  end
+
+  def page_heading(text)
+    content_for(:title, text)
+  end
 end
