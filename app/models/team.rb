f9dbcc443d58f95f526a7873ce3d667df7974913
Steve Hook <steve.hook@gmail.com>
2011-02-21 22:20:32 +0000

    Spec for setting the scrum_master of a team

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,12 @@
 class Team
   include Mongoid::Document
   field :name, :type => String
-  references_many :users
+  references_many :members, :class_name => 'User'
   referenced_in :product_owner, :class_name => 'User'
   referenced_in :scrum_master, :class_name => 'User'
+  
+  before_save :before_save
+  def before_save
+    self.scrum_master.team = self if self.scrum_master
+  end
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Team do
-  
+  context 'when creating a new team with a scrum master' do
+    let(:scrum_master) { User.create(:name => 'Arthur', :email => 'arthur@nocompany.com') }
+    let(:team) { Team.create(:name => 'Team 1') }
+    
+    it "the scrum master should be a member of the team" do
+      puts scrum_master.team
+      team.scrum_master = scrum_master
+      team.before_save
+      scrum_master.team.should_not be_nil
+      scrum_master.team.should == team
+      # TODO: Fix this assertion...
+      # team.members.length.should == 1
+    end
+  end
 end
