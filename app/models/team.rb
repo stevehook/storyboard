dadeaceef17de81ce3367274ebcd9cf7a1ff250c
Steve Hook <steve.hook@gmail.com>
2011-02-21 20:12:56 +0000

    Added associations between Team and User

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,7 @@
 class Team
   include Mongoid::Document
   field :name, :type => String
+  references_many :users
+  referenced_in :product_owner, :class_name => 'User'
+  referenced_in :scrum_master, :class_name => 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User
   include Mongoid::Timestamps
   field :name, :data_type => String
   field :email, :data_type => String
+  referenced_in :team
 
   validates :name, :email, :presence => true
 end
