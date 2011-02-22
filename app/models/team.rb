1118c19207f6fe777e4affc86b8c4814b91d1323
Steve Hook <steve.hook@gmail.com>
2011-02-22 20:41:57 +0000

    Implemented the team#new view

--- app/models/team.rb
+++ app/models/team.rb
@@ -7,6 +7,8 @@ class Team
   
   before_save :before_save
   def before_save
+    self.scrum_master_id = nil if self.scrum_master_id == ''
+    self.product_owner_id = nil if self.product_owner_id == ''
     self.scrum_master.team = self if self.scrum_master
   end
 end
