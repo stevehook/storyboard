1969a6c1cf77f2d3281d6ab1e2328b44f3e1e9e3
Steve Hook <steve.hook@gmail.com>
2011-03-03 20:13:11 +0000

    Use of html_safe in helpers

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -7,10 +7,14 @@ module StoriesHelper
       when :done then '#008'
       when :rejected then '#888'
     end
-    "<span class='statusBadge' style='background-color: #{colour};'>#{story.status}</span>"
+    "<span class='statusBadge' style='background-color: #{colour};'>#{story.status}</span>".html_safe
   end
   
   def priority_badge(story)
-    "<span class='priorityBadge'>#{story.priority}</span>"
+    "<span class='priorityBadge'>#{story.priority}</span>".html_safe
+  end
+  
+  def sprint_badge(story)
+    story.sprint.nil? ? "" : "<span class='priorityBadge'>sprint #{story.sprint.title}</span>".html_safe
   end
 end
