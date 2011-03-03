e64f4678db750ca81d09e26b0f1baa0627ea4352
Steve Hook <steve.hook@gmail.com>
2011-03-03 19:52:17 +0000

    Added styling to the status labels in the product backlog

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,2 +1,12 @@
 module StoriesHelper
+  def status_badge(story)
+    colour = case story.status.to_sym
+      when :open then '#800'
+      when :ready then '#E0981B'
+      when :committed then '#080'
+      when :done then '#008'
+      when :rejected then '#888'
+    end
+    "<span class='statusBadge' style='background-color: #{colour};'>#{story.status}</span>"
+  end
 end
