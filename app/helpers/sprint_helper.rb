1904ed9c798aff6ab23c79490947f2e8231c77c2
Steve Hook <steve.hook@gmail.com>
2011-03-06 19:24:15 +0000

    Added a burndown chart panel to the story#show view

--- app/helpers/sprint_helper.rb
+++ app/helpers/sprint_helper.rb
@@ -1,2 +1,6 @@
 module SprintHelper
+  def burndown_data(sprint)
+    # TODO: Get the real data from the statistics model
+    '[[[1, 300],[2,243],[3,221],[4,198],[4],[5],[6],[7],[8],[9]]]'
+  end
 end
