0f814eda0ba7db6205f89fc3c2c39d233e7ab7a5
Steve Hook <steve.hook@gmail.com>
2011-03-14 18:51:36 +0000

    Added password_hash and password_salt fields to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,24 @@ class User
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  attr_accessible :name, :email, :password, :password_confirmation
+  attr_accessor :password
   field :name, :data_type => String
   field :email, :data_type => String
+  field :password_hash,  :data_type => String
+  field :password_salt, :data_type => String
   referenced_in :team, :inverse_of => :members
 
-  validates :name, :email, :presence => true
-  
+  validates_presence_of :name, :email
+  validates_presence_of :password, :on => :create
+
+  def encrypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
+
   before_save :before_save
   def before_save
     self.team_id = nil if self.team_id == ''
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,21 @@
 require 'spec_helper'
 
 describe User do
-  context "when creating a new user with a name and email" do
-    let(:user) { User.create(:name => 'Fred', :email => 'fred@nocompany.com') }
+  context "when creating a new user with a name, email and password" do
+    let(:user) { User.create(:name => 'Fred', :email => 'fred@nocompany.com', :password => 'secret') }
     it "should be valid" do
       user.valid?.should be_true
       user.errors[:name].any?.should be_false
-      user.errors[:email].any?.should be_false      
+      user.errors[:email].any?.should be_false
     end
   end
 
   context "when creating a new user with a name but no email" do
-    let(:user) { User.create(:name => 'Fred') }
+    let(:user) { User.create(:name => 'Fred', :password => 'secret') }
     it "should be invalid" do
       user.valid?.should be_false
       user.errors[:name].any?.should be_false
-      user.errors[:email].any?.should be_true      
+      user.errors[:email].any?.should be_true
     end
   end
 end
