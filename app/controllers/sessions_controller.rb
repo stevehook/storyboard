7318e802e2df53b259d7a5ce88b1d6b739f5e373
Steve Hook <steve.hook@gmail.com>
2011-04-10 13:55:42 +0100

    Reinstanted sessions#destroy action (log_out)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,5 +12,10 @@ class SessionsController < ApplicationController
       render "new"
     end
   end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, :notice => "Logged out!"
+  end
 end
 
