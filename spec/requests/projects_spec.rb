3f4de349e05c488ceb3a8f628f1b3032e9d09254
Steve Hook <steve.hook@gmail.com>
2011-06-08 17:43:04 +0100

    Added spec for navigating from the Project#show action to #Release#show

--- spec/requests/projects_spec.rb
+++ spec/requests/projects_spec.rb
@@ -40,12 +40,30 @@ describe 'Projects', :type => :request do
       @project = Project.create(:title => 'Project X')
       Project.create(:title => 'Project Y')
       Project.create(:title => 'Project Z')
+      Release.create(:title => 'Version 1.0', :order => 1, :start_date => Time.utc(2010, 'feb', 12), :project => @project)
+      Release.create(:title => 'Version 2.0', :order => 2, :start_date => Time.utc(2011, 'jan', 3), :project => @project)
+      Release.create(:title => 'Version 3.0', :order => 3, :start_date => Time.utc(2011, 'oct', 4), :project => @project)
       visit project_path(@project)
     end
 
     it 'shows the project' do
       page.status_code.should == 200
       page.should have_content('Project - Project X')
+    end
+
+    it 'shows the releases' do
+      page.status_code.should == 200
+      page.should have_content('Version 1.0')
+      page.should have_content('Version 2.0')
+      page.should have_content('Version 3.0')
+      page.should_not have_content('Version 4.0')
+    end
+
+    it 'clicking on a release link should open the release#show' do
+      page.status_code.should == 200
+      link = find_link('Version 2.0')
+      link.click
+      page.should have_content('Release - Version 2.0')
     end
   end
 end
