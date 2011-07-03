04cbae1de05fb7cc6e3f07d74cb6cc309eb30cf5
Steve Hook <steve.hook@gmail.com>
2011-07-03 13:43:05 +0100

    Improvements to the appearance and information content of the Sprint#planning view

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -6,4 +6,8 @@ module StoriesHelper
   def sprint_badge(story)
     story.sprint.nil? ? "" : "<span class='priorityBadge'>sprint #{story.sprint.title}</span>".html_safe
   end
+
+  def points_badge(story)
+    "<span class='priorityBadge'>#{story.estimate} pts</span>".html_safe
+  end
 end
