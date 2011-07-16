00a4fc7e6710e035575d59710b9f392057587323
Steve Hook <steve.hook@gmail.com>
2011-07-16 18:09:48 +0100

    Need to come back to spec page_heading

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,7 +1,22 @@
 require 'spec_helper'
 
 describe ApplicationHelper do
-   it "should display the correct title" do
-     helper.page_title.should == 'Storyboard'
-   end
+  context "page title" do
+    before(:each) { @content_for_title = nil }
+
+    it "should display the correct title when the current project is nil" do
+      helper.page_title.should == 'Storyboard'
+    end
+
+    it "should display the correct title when there is a current project" do
+      @content_for_title = 'Test Project'
+      helper.page_title.should == 'Test Project &mdash; Storyboard'
+    end
+  end
+
+  # context "page heading" do
+  #   it "should ..." do
+  #     helper.page_heading(nil).should == ''
+  #   end
+  # end
 end
