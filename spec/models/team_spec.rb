1598434a62a4c0947c3b860e9384f0fc28e3b821
Steve Hook <steve.hook@gmail.com>
2011-09-03 19:42:42 +0100

    Implemented some specs to understand the Paranoia module in mongoid

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -6,13 +6,30 @@ describe Team do
     let(:team) { Team.create(:name => 'Team 1') }
     
     it "the scrum master should be a member of the team" do
-      puts scrum_master.team
       team.scrum_master = scrum_master
       team.before_save
       scrum_master.team.should_not be_nil
       scrum_master.team.should == team
-      # TODO: Fix this assertion...
-      # team.members.length.should == 1
+      team.members.length.should == 1
+    end
+  end
+
+  context 'when soft deleting a user' do
+    let(:arthur) { User.create(:name => 'Arthur', :email => 'arthur@nocompany.com') }
+
+    it "a newly created user should not be destroyed?" do
+      arthur.destroyed?.should be_false
+    end
+
+    it "a deleted user should be destroyed?" do
+      arthur.delete
+      arthur.destroyed?.should be_true
+    end
+
+    it "a restored user should not be destroyed?" do
+      arthur.delete
+      arthur.restore
+      arthur.destroyed?.should be_false
     end
   end
 end
